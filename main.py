992af1f21ff24db802fbafb21759951df5dc4542
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2025-04-17 20:28:16 +0700

    add function to check file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import tkinter as tk
 import datetime as datetime
+from datetime import timedelta as dt # Importing the datetime module to work with dates and times
+import os # Importing the os module to interact with the operating system
+
+
+
+def checkFile(filePath):
+    if os.path.exists(filePath):
+        print("File exists")
+    else:
+        print("File does not exist")
+        return False
+    return True
 
 root = tk.Tk()
 root.title("Photocopier")
@@ -10,10 +22,11 @@ label = tk.Label(root, text="Welcome to the Photocopier!\nThis will help you cop
 label.config(font=("Arial", 12))
 label.config(bg="lightblue")
 label.pack(pady=20)
-currentDate = tk.IntVar()
-currentDate.set(datetime.datetime.today().strftime("%Y-%m-%d"))
-print(currentDate)
-dateLabel = tk.Label(root, text="Current Date: 2023-10-01")
+currentDate = tk.StringVar()
+currentDate.set((datetime.datetime.today() - dt(1)).strftime("%Y%m%d"))
+print(currentDate.get())
+dateLabel = tk.Label(root, text="Yesterday Date: "+ currentDate.get())
+checkFile("C:/Users/gunaw/AppData/Local/Packages/Microsoft.BingWallpaper_8wekyb3d8bbwe/LocalState/images/Bing/20250417_bing.jpg")
 dateLabel.config(font=("Arial", 10))
 dateLabel.config(bg="lightblue")
 dateLabel.pack(pady=5)
@@ -26,5 +39,6 @@ button2.pack(pady=10)
 
 print("Welcome to the Photocopier!")
 root.mainloop()
+
 
 
