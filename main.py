3ae24368e48ecc6c33b0019450854c9f7385732a
Stephen Gunawan Susilo <gunawanstephen@yahoo.com>
2025-04-19 18:19:02 +0700

    finish the basic function

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import tkinter as tk
 import datetime as datetime
 from datetime import timedelta as dt # Importing the datetime module to work with dates and times
 import os # Importing the os module to interact with the operating system
+import shutil # Importing the shutil module to perform high-level file operations
 
 
 
 def checkFile(filePath):
     if os.path.exists(filePath):
-        print("File exists")
+        # copy file to the destination folder
+        destination = "D:/gambar2/tempwallpaper/"
+        # Check if the destination folder exists, if not create it
+        if not os.path.exists(destination):
+            os.makedirs(destination)
+        # Copy the file to the destination folder
+        # Use os.system to copy the file (this is a simple way to copy files in Python)
+        # os.system(f'copy "{filePath}" "{destination}"')
+        shutil.copyfile(filePath, destination+"wallpaper.jpg")
+        print(filePath + " copied to " + destination)
     else:
         print("File does not exist")
         return False
@@ -26,12 +36,10 @@ currentDate = tk.StringVar()
 currentDate.set((datetime.datetime.today() - dt(1)).strftime("%Y%m%d"))
 print(currentDate.get())
 dateLabel = tk.Label(root, text="Yesterday Date: "+ currentDate.get())
-checkFile("C:/Users/gunaw/AppData/Local/Packages/Microsoft.BingWallpaper_8wekyb3d8bbwe/LocalState/images/Bing/20250417_bing.jpg")
+checkFile("C:/Users/gunaw/AppData/Local/Packages/Microsoft.BingWallpaper_8wekyb3d8bbwe/LocalState/images/Bing/"+currentDate.get()+"_bing.jpg")
 dateLabel.config(font=("Arial", 10))
 dateLabel.config(bg="lightblue")
 dateLabel.pack(pady=5)
-button = tk.Button(root, text="Start", command=lambda: print("Starting Photocopier..."))
-button.pack(pady=10)
 button2 = tk.Button(root, text="Exit", command=root.quit)
 button2.pack(pady=10)
 
